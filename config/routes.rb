db21d945753ea31a6f38a60968d0ed5fbf644623
Santiago <Mesa>
2023-03-07 15:46:25 -0500

    ejercicio iniciado y logica basica dreada hasta el metodo edit de un crud basico para un comercio electronico.

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post 'products', to: 'products#create'
+  get 'products/new', to: 'products#new', as: :new_product
+  get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
+  get '/products/:id/edit', to: 'products#edit', as: :edit_product
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,49 @@
+require "test_helper"
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test 'render a list of products' do
+    get products_path
+    assert_response :success
+    assert_select '.product', 2
+  end
+
+  test 'render a detailed product page' do
+    get product_path(products(:ps4))
+    assert_response :success
+    assert_select '.title', 'PS4 Fat'
+    assert_select '.description', 'PS4 en buen estado'
+    assert_select '.price', '$150'
+  end
+
+  test 'render a new product form' do
+    get new_product_path
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'create a new product' do
+    assert_difference 'Product.count', 1 do
+      post products_path, params: { product: { title: 'PS5', description: 'PS5 en buen estado', price: 400 } }
+    end
+    assert_redirected_to products_path
+    assert_equal 'Tu producto se ha creado correctamente.', flash[:notice]
+  end
+
+  test 'render a new product form with errors' do
+    post products_path, params: { product: { title: '', description: '', price: '' } }
+    assert_response :unprocessable_entity
+    assert_select 'form'
+  end
+
+  test 'render a edit product form' do
+    get edit_product_path(products(:ps4))
+    assert_response :success
+    assert_select 'form'
+  end
+  
+
+end
