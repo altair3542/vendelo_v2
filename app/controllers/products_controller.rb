7802f4e0d223241f39bdc8e6125d12524ded3de4
Santiago <Mesa>
2023-03-07 16:19:08 -0500

    se hace la adicion del metodo eliminar y su respectiva prueba

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,7 +32,13 @@ class ProductsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-  
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente.', status: :see_other
+  end
+
 
   private
 
